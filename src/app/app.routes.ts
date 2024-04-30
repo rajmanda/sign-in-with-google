e2019ba421cc2b04eb6204940256758aeaad20cb
Raj Manda <raj.manda@gmail.com>
2024-04-30 08:28:17 -0400

    Added Auth Guard to dashboard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ErrorComponent } from './error/error.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
   { path: 'error', component: ErrorComponent },
   { path: '', component: LoginComponent },
 
